a64938181660eab0366425dac92b906666a57c92
Andrew Alyamovsky <andrew.alyamovsky@gmail.com>
2024-05-03 22:30:00 +0200

    Add fighter class

---
+++ core/src/com/alyamovsky/sfs/model/Fighter.java
@@ -0,0 +1,149 @@
+package com.alyamovsky.sfs.model;
+
+import com.alyamovsky.sfs.resource.Assets;
+import com.alyamovsky.sfs.resource.Constants;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+import org.jetbrains.annotations.NotNull;
+
+public class Fighter {
+    private static final int FRAME_ROWS = 2;
+    private static final int FRAME_COLUMNS = 3;
+    private static final float MOVEMENT_SPEED = 10f;
+    private static final float MAX_LIFE = 100f;
+    private static final float HIT_STRENGTH = 5f;
+    private static final float BLOCK_DAMAGE_FACTOR = 0.2f;
+
+    private String name;
+    private Color color;
+    private State state;
+    private float stateTime;
+    private State renderState;
+    private float renderStateTime;
+    private final Vector2 position = new Vector2();
+    private final Vector2 movementDirection = new Vector2();
+    private float life;
+    private Facing facing;
+    private boolean madeContact;
+
+    private Animation<TextureRegion> blockAnimation;
+    private Animation<TextureRegion> hurtAnimation;
+    private Animation<TextureRegion> idleAnimation;
+    private Animation<TextureRegion> kickAnimation;
+    private Animation<TextureRegion> loseAnimation;
+    private Animation<TextureRegion> punchAnimation;
+    private Animation<TextureRegion> walkAnimation;
+    private Animation<TextureRegion> winAnimation;
+
+    public Fighter(AssetManager assetManager, String name, Color color) {
+        this.name = name;
+        this.color = color;
+
+        blockAnimation = new Animation<>(0.05f, getAnimationFrames(assetManager.get(Assets.BLOCK_SPRITE_SHEET)));
+        hurtAnimation = new Animation<>(0.03f, getAnimationFrames(assetManager.get(Assets.HURT_SPRITE_SHEET)));
+        idleAnimation = new Animation<>(0.1f, getAnimationFrames(assetManager.get(Assets.IDLE_SPRITE_SHEET)));
+        kickAnimation = new Animation<>(0.05f, getAnimationFrames(assetManager.get(Assets.KICK_SPRITE_SHEET)));
+        loseAnimation = new Animation<>(0.05f, getAnimationFrames(assetManager.get(Assets.LOSE_SPRITE_SHEET)));
+        punchAnimation = new Animation<>(0.05f, getAnimationFrames(assetManager.get(Assets.PUNCH_SPRITE_SHEET)));
+        walkAnimation = new Animation<>(0.08f, getAnimationFrames(assetManager.get(Assets.WALK_SPRITE_SHEET)));
+        winAnimation = new Animation<>(0.05f, getAnimationFrames(assetManager.get(Assets.WIN_SPRITE_SHEET)));
+    }
+
+    public void getReady(float x, float y) {
+        state = renderState = State.IDLE;
+        stateTime = renderStateTime = 0f;
+        position.set(x, y);
+        movementDirection.setZero();
+        life = MAX_LIFE;
+        madeContact = false;
+    }
+
+    /**
+     * It looks like it does not belong here, refactor
+     */
+    public void render(SpriteBatch spriteBatch) {
+        TextureRegion currentFrame = null;
+        switch (renderState) {
+            case BLOCK:
+                currentFrame = blockAnimation.getKeyFrame(renderStateTime, true);
+                break;
+            case HURT:
+                currentFrame = hurtAnimation.getKeyFrame(renderStateTime, false);
+                break;
+            case IDLE:
+                currentFrame = idleAnimation.getKeyFrame(renderStateTime, true);
+                break;
+            case KICK:
+                currentFrame = kickAnimation.getKeyFrame(renderStateTime, false);
+                break;
+            case LOSE:
+                currentFrame = loseAnimation.getKeyFrame(renderStateTime, false);
+                break;
+            case PUNCH:
+                currentFrame = punchAnimation.getKeyFrame(renderStateTime, false);
+                break;
+            case WALK:
+                currentFrame = walkAnimation.getKeyFrame(renderStateTime, true);
+                break;
+            case WIN:
+                currentFrame = winAnimation.getKeyFrame(renderStateTime, false);
+                break;
+        }
+
+        spriteBatch.setColor(color);
+        spriteBatch.draw(currentFrame,
+                         position.x,
+                         position.y,
+                         currentFrame.getRegionWidth() * Constants.WORLD_SCALE,
+                         currentFrame.getRegionHeight() * Constants.WORLD_SCALE
+        );
+        spriteBatch.setColor(Color.WHITE);
+    }
+
+    public void update(float deltaTime) {
+        stateTime += deltaTime;
+
+        if (deltaTime > 0) {
+            renderState = state;
+            renderStateTime = stateTime;
+        }
+    }
+
+    private TextureRegion @NotNull [] getAnimationFrames(Texture spriteSheet) {
+        TextureRegion[][] tmp = TextureRegion.split(spriteSheet,
+                                                    spriteSheet.getWidth() / FRAME_COLUMNS,
+                                                    spriteSheet.getHeight() / FRAME_ROWS
+        );
+        TextureRegion[] frames = new TextureRegion[FRAME_ROWS * FRAME_COLUMNS];
+        int index = 0;
+
+        for (int row = 0; row < FRAME_ROWS; row++) {
+            for (int column = 0; column < FRAME_COLUMNS; column++) {
+                frames[index++] = tmp[row][column];
+            }
+        }
+
+        return frames;
+    }
+
+    private enum State {
+        BLOCK,
+        HURT,
+        IDLE,
+        KICK,
+        LOSE,
+        PUNCH,
+        WALK,
+        WIN,
+    }
+
+    private enum Facing {
+        LEFT,
+        RIGHT,
+    }
+}
--- core/src/com/alyamovsky/sfs/screen/GameScreen.java
+++ core/src/com/alyamovsky/sfs/screen/GameScreen.java
@@ -1,30 +1,53 @@
 package com.alyamovsky.sfs.screen;
 
 import com.alyamovsky.sfs.SFS;
+import com.alyamovsky.sfs.model.Fighter;
 import com.alyamovsky.sfs.resource.Assets;
 import com.alyamovsky.sfs.resource.Constants;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameScreen implements Screen {
-    private final SFS game;
+    private static final float PLAYER_1_START_POSITION_X = 16f;
+    private static final float PLAYER_1_START_POSITION_Y = 15f;
+    private static final float PLAYER_2_START_POSITION_X = 51f;
+    private static final float PLAYER_2_START_POSITION_Y = 15f;
+
+    private final SFS sfs;
     private final Viewport viewport;
     private Texture backgroundTexture;
     private Texture frontRopesTexture;
+    public Fighter player1;
+    public Fighter player2;
 
-    public GameScreen(SFS game) {
-        this.game = game;
-        this.viewport = new ExtendViewport(Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT * 0.75f, Constants.WORLD_WIDTH, Constants.WORLD_HEIGHT);
+    public GameScreen(SFS sfs) {
+        this.sfs = sfs;
+        this.viewport = new ExtendViewport(
+                Constants.WORLD_WIDTH,
+                Constants.WORLD_HEIGHT * 0.75f,
+                Constants.WORLD_WIDTH,
+                Constants.WORLD_HEIGHT
+        );
 
         createGameArea();
+        player1.getReady(PLAYER_1_START_POSITION_X, PLAYER_1_START_POSITION_Y);
+        player2.getReady(PLAYER_2_START_POSITION_X, PLAYER_2_START_POSITION_Y);
     }
 
     private void createGameArea() {
-        backgroundTexture = game.assets.manager.get(Assets.BACKGROUND_TEXTURE);
-        frontRopesTexture = game.assets.manager.get(Assets.FRONT_ROPES_TEXTURE);
+        backgroundTexture = sfs.assets.manager.get(Assets.BACKGROUND_TEXTURE);
+        frontRopesTexture = sfs.assets.manager.get(Assets.FRONT_ROPES_TEXTURE);
+        player1 = new Fighter(sfs.assets.manager, "Player", new Color(1f, 0.2f, 0.2f, 1f));
+        player2 = new Fighter(sfs.assets.manager, "Opponent", new Color(0.25f, 0.7f, 1f, 1f));
+    }
+
+    public void update(float delta) {
+        player1.update(delta);
+        player2.update(delta);
     }
 
     @Override
@@ -35,10 +58,24 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         ScreenUtils.clear(0, 0, 0, 1);
 
-        game.batch.setProjectionMatrix(viewport.getCamera().combined);
-        game.batch.begin();
-        game.batch.draw(backgroundTexture, 0, 0, backgroundTexture.getWidth() * Constants.WORLD_SCALE, backgroundTexture.getHeight() * Constants.WORLD_SCALE);
-        game.batch.end();
+        update(delta);
+
+        sfs.batch.setProjectionMatrix(viewport.getCamera().combined);
+        sfs.batch.begin();
+        sfs.batch.draw(
+                backgroundTexture,
+                0,
+                0,
+                backgroundTexture.getWidth() * Constants.WORLD_SCALE,
+                backgroundTexture.getHeight() * Constants.WORLD_SCALE
+        );
+        renderFighters();
+        sfs.batch.end();
+    }
+
+    private void renderFighters() {
+        player1.render(sfs.batch);
+        player2.render(sfs.batch);
     }
 
     @Override
